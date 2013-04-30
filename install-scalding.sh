4a7a840b57bc71ca983634cb7a781ee27753f7f6
Zhaojun Zhang <zhangzhaojun@gmail.com>
2013-04-30 12:32:43 -0400

    fix the installation of scala and scalding

--- install-scalding.sh
+++ install-scalding.sh
@@ -1,5 +1,5 @@
 cd software
-git clone git@github.com:twitter/scalding.git -b branch/0.9.0
+git clone git@github.com:twitter/scalding.git
 cd scalding
 sbt update
 sbt test     # runs the tests; if you do 'sbt assembly' below, these tests, which are long, are repeated
